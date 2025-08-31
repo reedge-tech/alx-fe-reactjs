2b775b7318a82d20a2fa09b608f63d5255b06a8e
Reedge Dangana <danganareedge@gmail.com>
2025-08-31 16:04:35 +0300

    feat: add Header, MainContent, Footer components and integrate into App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage';
 function App() {
@@ -9,6 +13,9 @@ function App() {
   return (
     <>
         <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <Footer />
     </>
   )
 }
