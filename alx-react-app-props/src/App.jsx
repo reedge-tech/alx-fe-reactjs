bbf34dad0f866f4ef01d0e089fbaece43be7d0c3
Reedge Dangana <danganareedge@gmail.com>
2025-09-04 20:03:30 +0300

    Refactor prop drilling using Context API in UserDetails.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,12 +1,12 @@
+import ProfilePage from './ProfilePage';
 import UserContext from './UserContext';
-import UserProfile from './UserProfile';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
-      <UserProfile />
+      <ProfilePage />
     </UserContext.Provider>
   );
 }
