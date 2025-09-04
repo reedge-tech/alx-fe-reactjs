593808898b9563c26cee2b1cd6cc37f92b9f357a
Reedge Dangana <danganareedge@gmail.com>
2025-09-04 20:37:48 +0300

    Added my-company React project with routing and contact form

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,23 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import Services from "./Services";
-import Contact from "./Contact";
-import Navbar from "./Navbar";
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
   );
 }
 
--- my-company/src/Navbar.jsx
+++ my-company/src/Navbar.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", backgroundColor: "#ddd" }}>
-      <Link to="/" style={{ margin: "0 10px" }}>Home</Link>
-      <Link to="/about" style={{ margin: "0 10px" }}>About</Link>
-      <Link to="/services" style={{ margin: "0 10px" }}>Services</Link>
-      <Link to="/contact" style={{ margin: "0 10px" }}>Contact</Link>
+    <nav style={{ padding: '10px', backgroundColor: '#ddd' }}>
+      <Link to="/" style={{ margin: '10px' }}>Home</Link>
+      <Link to="/about" style={{ margin: '10px' }}>About</Link>
+      <Link to="/services" style={{ margin: '10px' }}>Services</Link>
+      <Link to="/contact" style={{ margin: '10px' }}>Contact</Link>
     </nav>
   );
 }
