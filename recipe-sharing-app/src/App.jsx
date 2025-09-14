8b6bca71f733d8ee3d752ea27086d8c56821f5e3
Reedge Dangana <danganareedge@gmail.com>
2025-09-14 16:21:22 +0300

    Wrap App.jsx in BrowserRouter (Router) for checker

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <AddRecipeForm />
-              <RecipeList />
-            </div>
-          }
-        />
-        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <AddRecipeForm />
+                <RecipeList />
+              </div>
+            }
+          />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
